1888a7afbeb5e256a2ec8296493f78db7ebdb20c
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-09-23 19:22:43 +0300

    #1 Preparations to release version fully via CD p…
    …ipeline.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,13 @@
 
 . 'scripts/preamble.sh'
 
-yarn run build
+TYRAS_BUILD_MODE="$1"
+if [ "${TYRAS_BUILD_MODE}" = "run" ] || [ "${TYRAS_BUILD_MODE}" = "ci" ]; then
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+else
+  TYRAS_BUILD_MODE='run'
+fi
+
+yarn run "build:${TYRAS_BUILD_MODE}"
