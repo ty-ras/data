1bf4ccaccfc7c7d06755f4c180faba986055d718
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-16 20:50:13 +0300

    #12 Taking new scripts into use, and migrating aw…
    …ay from using .d.ts files within source code.

--- data-backend/src/index.ts
+++ data-backend/src/index.ts
@@ -1,43 +1,8 @@
-// Really waiting for that "export type *": https://github.com/microsoft/TypeScript/issues/37238
-// If we just do "export * from", our index.[m]js file ends up with "export" statement as well, thus causing runtime errors.
-// Another option is rename .d.ts files into .ts files and end up with a bunch of empty .[m]js files and index.[m]js exporting those - not very optimal either.
-export type { StateValidator, StateValidatorResult } from "./state";
-export type {
-  RuntimeAnyURLData,
-  URLParameterValidationAdditionalMetadata,
-  URLParameterValidatorSpec,
-  URLParameterValidatorSpecMetadata,
-  URLParameterValidators,
-  URLParameterValue,
-} from "./url";
-export type {
-  HeaderDataValidatorSpec,
-  HeaderDataValidatorSpecMetadata,
-  HeaderDataValidators,
-  HeaderValidationAdditionalMetadata,
-  RequestHeaderDataValidatorSpec,
-  RequestHeaderDataValidatorSpecMetadata,
-  RequestHeaderDataValidators,
-  ResponseHeaderDataValidatorSpec,
-  ResponseHeaderDataValidatorSpecMetadata,
-  ResponseHeaderDataValidators,
-  RuntimeAnyHeaders,
-} from "./headers";
-export type {
-  QueryDataValidatorSpecMetadata,
-  QueryDataValidators,
-  QueryValidationAdditionalMetadata,
-  QueryValidatorSpec,
-  RuntimeAnyQuery,
-} from "./query";
-export type {
-  RuntimeAnyStringData,
-  StringDataValidatorSpec,
-  StringDataValidatorSpecMetadata,
-  StringDataValidators,
-  WithDecoder,
-  WithEncoder,
-} from "./string";
+export type * from "./state";
+export type * from "./url";
+export type * from "./headers";
+export type * from "./query";
+export type * from "./string";
 export * from "./errors";
 export * from "./request-body";
 export * from "./response-body";
