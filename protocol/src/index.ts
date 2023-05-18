9625786970ec0048344e7d515d209742add1b0c4
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2023-05-18 12:16:24 +0300

    #12 All protocol package file are types-only.

--- protocol/src/index.ts
+++ protocol/src/index.ts
@@ -1,5 +1,5 @@
 /**
  * @file This is entrypoint file for this package, exporting all non-internal files.
  */
-export type * from "./spec";
-export type * from "./encoding";
+export type * from "./spec.types";
+export type * from "./encoding.types";
